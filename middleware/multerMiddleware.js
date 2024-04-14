83432274c583c491ae8cac5e77f25c7153ccf6e3
Jason Woo <jaswoo73@gmail.com>
2024-04-14 21:30:32 +0800

    fix update user image functionality

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,15 +1,16 @@
 import multer from "multer";
+import DataURIParser from "datauri/parser.js";
+import path from "path";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads");
-  },
-  filename: function (req, file, cb) {
-    const fileName = file.originalname;
-    cb(null, fileName);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
+
+const parser = new DataURIParser();
+
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalname).toString();
+  return parser.format(fileExtension, file.buffer).content;
+};
 
 export default upload;
